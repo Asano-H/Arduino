07986cfcbe802f02f5e1228d8eedca665e5a545b
Asano-H <princessknk0406@gmail.com>
2017-11-07 23:02:09 +0900

    add

--- sketch_nov07b/sketch_nov07b.ino
+++ sketch_nov07b/sketch_nov07b.ino
@@ -1,6 +1,16 @@
 #define myLED_1 13
+#define myLED_2 12
+#define myLED_3 10
+#define myLED_4 8
+#define switch 3
+
+int n=0;
+int num;
 void setup() {
   pinMode(myLED_1, OUTPUT);
+  pinMode(myLED_2, OUTPUT);
+  pinMode(myLED_3, OUTPUT);
+  pinMode(myLED_4, OUTPUT);
 
 }
 
