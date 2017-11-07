612c6018386e29f2425798f2e31d2d867254e7ce
Asano-H <princessknk0406@gmail.com>
2017-11-07 21:14:01 +0900

    commit

---
+++ sketch_nov07b/sketch_nov07b.ino
@@ -0,0 +1,12 @@
+#define myLED_1 13
+void setup() {
+  pinMode(myLED_1, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(myLED_1, HIGH);
+  delay(1000);
+  digitalWrite(myLED_1, LOW);
+  delay(1000);
+}
