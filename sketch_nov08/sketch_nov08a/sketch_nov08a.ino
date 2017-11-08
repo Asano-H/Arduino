843a4ae31701f568800a0f64acf84bc2a998f1f3
Asano-H <princessknk0406@gmail.com>
2017-11-08 17:54:10 +0900

    commit

---
+++ sketch_nov08/sketch_nov08a/sketch_nov08a.ino
@@ -0,0 +1,13 @@
+#define myLED_1 11
+int n;
+void setup() {
+  pinMode(myLED_1, OUTPUT);  
+}
+
+void loop() {
+  for(n=0;n<256;n++){
+    analogWrite(myLED_1,n);
+    delay(10);
+  }
+  digitalWrite(myLED_1, LOW);
+}
