03deef58109fbe19fec8d02430674caa20c84638
Asano-H <princessknk0406@gmail.com>
2017-11-08 17:54:28 +0900

    commit

---
+++ sketch_nov08/sketch_nov08b/sketch_nov08c.ino
@@ -0,0 +1,22 @@
+#define myLED_1 11
+#define switch 3
+int n;
+int sw=0;
+void setup() {
+  pinMode(myLED_1, OUTPUT);
+  pinMode(switch,INPUT);
+}
+
+void loop() {
+  sw = digitalRead(switch);
+  if(sw==HIGH){
+    for(n=256;n>0;n--){
+      analogWrite(myLED_1,n);
+      delay(10);
+    }
+    digitalWrite(myLED_1, LOW);
+    sw = digitalRead(switch);
+  }else{
+    digitalWrite(myLED_1,LOW);
+    }
+}
