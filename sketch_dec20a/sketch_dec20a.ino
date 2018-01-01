da35364275bf6cb6cad97955cd7b0089c5ac9777
Asano-H <princessknk0406@gmail.com>
2018-01-01 20:24:42 +0900

    add

--- sketch_dec20a/sketch_dec20a.ino
+++ sketch_dec20a/sketch_dec20a.ino
@@ -1,18 +1,23 @@
 #include <Servo.h>
 
 Servo myservo1;
+Servo myservo2;
+Servo myservo3;
 
 
 void setup()
 {
-	myservo1.attach(11);
+	myservo1.attach(11);	/*右腕*/
+	myservo2.attach(12);	/*左腕*/
+
 }
 
 void loop()
 {
 	myservo1.write(0);
 	delay(1000);
-	myservo1.write(90);
+	myservo1.write(180);
 	delay(1000);
+	myservo
 }
 
