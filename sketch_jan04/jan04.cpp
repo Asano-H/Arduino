12b6658f8032f57d272756338f8630ea1c301d20
Asano-H <princessknk0406@gmail.com>
2018-01-05 00:08:12 +0900

    commit

---
+++ sketch_jan04/jan04.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "jan04.h"
+
+/*コンストラクタ*/
+pikapika::pikapika(int time,int pin){
+	myled_time = time;
+	myled_pin = pin;
+	pinMode(myled_pin, OUTPUT);
+}
+
+void pikapika::tenmetsu(time){
+	digitalWrite(myled_pin, HIGH);
+	delay(time);
+	digitalWrite(myled_pin, LOW);
+	delay(time);
+
+}
---
+++ sketch_jan04/sketch_jan04.ino
@@ -0,0 +1,12 @@
+#include <jan04.h>
+
+pikapika my_led1(1000,10);
+
+void setup()
+{
+}
+
+void loop()
+{
+	my_led1.tenmetsu();
+}
