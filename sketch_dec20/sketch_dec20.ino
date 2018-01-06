4b77f21917c3d2fe304f47e4c4fa4919f911fd7d
Asano-H <princessknk0406@gmail.com>
2018-01-06 22:18:45 +0900

    pico_message add

--- sketch_dec20/sketch_dec20.ino
+++ sketch_dec20/sketch_dec20.ino
@@ -5,117 +5,136 @@
 #define pico_pc3 8
 #define pico_sleep  2
 
-void picoms_00(){
-	digitalWrite(pico_pc3, LOW);	/*0000*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, LOW);
+void pico_message(int ms_number){
+	int i;
+	int pico_pc[3]=0;
+
+	/*10-2*/
+	for(i=0;ms_number>0;i++){
+		pico_pc[i] = ms_number %2;
+		ms_number = ms_number /2;
+	}
+
+	while(i>0){
+		digitalWrite(i+5, pico_pc[i] ? HIGH : LOW );
+	}
+
+
+
 }
 
-void picoms_01(){
-	digitalWrite(pico_pc3, LOW);	/*0001*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, HIGH);
-}
 
-void picoms_02(){
-	digitalWrite(pico_pc3, LOW);	/*0010*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_00(){
+// 	digitalWrite(pico_pc3, LOW);	/*0000*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_03(){
-	digitalWrite(pico_pc3, LOW);	/*0011*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_01(){
+// 	digitalWrite(pico_pc3, LOW);	/*0001*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_04(){
-	digitalWrite(pico_pc3, LOW);	/*0100*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_02(){
+// 	digitalWrite(pico_pc3, LOW);	/*0010*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_05(){
-	digitalWrite(pico_pc3, LOW);	/*0101*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_03(){
+// 	digitalWrite(pico_pc3, LOW);	/*0011*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_06(){
-	digitalWrite(pico_pc3, LOW);	/*0110*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_04(){
+// 	digitalWrite(pico_pc3, LOW);	/*0100*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_07(){
-	digitalWrite(pico_pc3, LOW);	/*0111*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_05(){
+// 	digitalWrite(pico_pc3, LOW);	/*0101*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_08(){
-	digitalWrite(pico_pc3, HIGH);	/*1000*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_06(){
+// 	digitalWrite(pico_pc3, LOW);	/*0110*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_09(){
-	digitalWrite(pico_pc3, HIGH);	/*1001*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_07(){
+// 	digitalWrite(pico_pc3, LOW);	/*0111*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_10(){
-	digitalWrite(pico_pc3, HIGH);	/*1010*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_08(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1000*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_11(){
-	digitalWrite(pico_pc3, HIGH);	/*1011*/
-	digitalWrite(pico_pc2, LOW);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_09(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1001*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_12(){
-	digitalWrite(pico_pc3, HIGH);	/*1100*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_10(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1010*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_13(){
-	digitalWrite(pico_pc3, HIGH);	/*1101*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, LOW);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_11(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1011*/
+// 	digitalWrite(pico_pc2, LOW);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
-void picoms_14(){
-	digitalWrite(pico_pc3, HIGH);	/*1110:一緒にチョコレート食べませんか*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, LOW);
-}
+// void picoms_12(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1100*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, LOW);
+// }
 
-void picoms_15(){
-	digitalWrite(pico_pc3, HIGH);	/*1111*/
-	digitalWrite(pico_pc2, HIGH);
-	digitalWrite(pico_pc1, HIGH);
-	digitalWrite(pico_pc0, HIGH);
-}
+// void picoms_13(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1101*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, LOW);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
+
+// void picoms_14(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1110:一緒にチョコレート食べませんか*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, LOW);
+// }
+
+// void picoms_15(){
+// 	digitalWrite(pico_pc3, HIGH);	/*1111*/
+// 	digitalWrite(pico_pc2, HIGH);
+// 	digitalWrite(pico_pc1, HIGH);
+// 	digitalWrite(pico_pc0, HIGH);
+// }
 
 void setup()
 {
