d20136d66c4caaf1690f682a257c3893b119af18
Asano-H <princessknk0406@gmail.com>
2018-04-20 00:22:38 +0900

    add color

--- dynamic_lighting/dynamic_lighting.ino
+++ dynamic_lighting/dynamic_lighting.ino
@@ -1,28 +1,85 @@
 #define myLED_right	2
 #define myLED_left	3
 #define myLED_red	5
-#define myLED_blue	6
-#define myLED_green	7
+#define myLED_green	6
+#define myLED_blue	7
+
+#define my_swith 8
+
+int color;
+int n=0;
+
+void LED_red(){
+	digitalWrite(myLED_right, HIGH);
+	digitalWrite(myLED_left, HIGH);
+	digitalWrite(myLED_red,HIGH);
+
+	for(int i=0;n==LOW;i++){
+		digitalWrite(myLED_left,LOW);
+		digitalWrite(myLED_left, HIGH);
+		digitalWrite(myLED_right,LOW);
+		digitalWrite(myLED_right, HIGH);
+	}
+
+}
+
+void LED_green(){
+	digitalWrite(myLED_right, HIGH);
+	digitalWrite(myLED_left, HIGH);
+	digitalWrite(myLED_green,HIGH);
+
+	for(int i=0;n==LOW;i++){
+		digitalWrite(myLED_left,LOW);
+		digitalWrite(myLED_left, HIGH);
+		digitalWrite(myLED_right,LOW);
+		digitalWrite(myLED_right, HIGH);
+	}
+
+}
+
+void LED_green(){
+	digitalWrite(myLED_right, HIGH);
+	digitalWrite(myLED_left, HIGH);
+	digitalWrite(myLED_blue,HIGH);
+
+	for(int i=0;n==LOW;i++){
+		digitalWrite(myLED_left,LOW);
+		digitalWrite(myLED_left, HIGH);
+		digitalWrite(myLED_right,LOW);
+		digitalWrite(myLED_right, HIGH);
+	}
+
+}
+
 
 void setup() {
 	pinMode(myLED_right, OUTPUT);
 	pinMode(myLED_left, OUTPUT);
 	pinMode(myLED_red, OUTPUT);
-	pinMode(myLED_blue, OUTPUT);
 	pinMode(myLED_green, OUTPUT);
+	pinMode(myLED_blue, OUTPUT);
 
-	digitalWrite(myLED_right, HIGH);
-	digitalWrite(myLED_left, HIGH);
-	digitalWrite(myLED_red,HIGH);
-	delay(10);
+	n=digitalRead(my_switch);
+	if(n==HIGH){
+		color = random(2);
+		switch(color){
+			case 0: LED_red(); break;
+			case 1: LED_green(); break;
+			case 2: LED_blue(); break;
+		}
+
+
+	}
+
+
+
+	LED_red();
+
   
 }
 
 void loop() {
-	digitalWrite(myLED_left,LOW);
-	digitalWrite(myLED_left, HIGH);
-	digitalWrite(myLED_right,LOW);
-	digitalWrite(myLED_right, HIGH);
+
 
 
 }
